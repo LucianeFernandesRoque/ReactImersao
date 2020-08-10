d82af932b6395ccac5642fb628b4de66cb1066ad
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2020-08-10 01:26:55 -0300

    update

--- src/Pages/Video/index.js
+++ src/Pages/Video/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TagCovid from '../../components/TagCovid';
 
 function Video() {
-  return <TagCovid>Deu bom</TagCovid>;
+  return <TagCovid>Deu om</TagCovid>;
 }
 
 export default Video;
